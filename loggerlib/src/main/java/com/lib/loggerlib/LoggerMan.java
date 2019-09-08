795281adb4e179cfb7955b974b444f7fcc4a0138
akshayvurs <akshayurs123@gmail.com>
2019-09-09 00:10:40 +0530

    library commit

---
+++ loggerlib/src/main/java/com/lib/loggerlib/LoggerMan.java
@@ -0,0 +1,14 @@
+package com.lib.loggerlib;
+
+import android.util.Log;
+
+public class LoggerMan {
+
+    public void recordLoggerMan(String tag , String value){
+
+        Log.e(tag,value);
+
+    }
+
+
+}
